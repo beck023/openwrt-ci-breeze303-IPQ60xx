67df98cecf506fa933abbc9ed3867249de356dee
beck023 <44996115+beck023@users.noreply.github.com>
2024-10-30 14:48:56 +0800

    Update my-diy.sh

--- my-diy.sh
+++ my-diy.sh
@@ -1,1 +1,22 @@
 
+#!/bin/bash
+
+# 修改默认IP
+# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.67.1/g' package/base-files/files/bin/config_generate
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+git_sparse_clone master https://github.com/sundaqiang/openwrt-packages luci-app-wolplus
+
+#./scripts/feeds update -a
+#./scripts/feeds install -a
